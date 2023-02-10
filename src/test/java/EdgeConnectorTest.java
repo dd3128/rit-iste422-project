573cfebc09e023a1aa8c30621bf8fbac85ffd37c
Yunhao Dong <dd3128@rit.edu>
2023-02-09 21:32:22 -0500

    fixed sh and some files

--- src/test/java/EdgeConnectorTest.java
+++ src/test/java/EdgeConnectorTest.java
@@ -1,10 +1,7 @@
-package src.test.java;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-
-import src.main.java.EdgeConnector;
 
 public class EdgeConnectorTest {
 	EdgeConnector testObj;
